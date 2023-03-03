f22e32fa6d7d36e86cfb16e119bdae85231d2de3
xyeison2010 <leonardohuacahidalgo@gmail.com>
2023-03-03 11:48:13 -0500

    load

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,31 @@
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./Footer/Footer";
+import Navbar from "./Navbar/Navbar";
+import Home from "../pages/Home/Home";
+import ProductDetail from "../pages/ProductDetail/ProductDetail";
+import Products from "../pages/Products/Products";
+import "./app.scss";
 
-import { Outlet, BrowserRouter, Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer/Footer";
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./pages/Home/Home";
-import ProductDetail from "./pages/ProductDetail/ProductDetail"
-import Products from "./pages/Products/Products";
-import "./app.scss"
-
-import { CartProvider } from "./context/CartContext";
+import { CartProvider } from "../context/CartContext";
 
 function App() {
   return (
     <CartProvider>
       <>
         <BrowserRouter>
-
           <div className="app">
             <Navbar />
-            <Outlet />
-
           </div>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/products/" element={<Products />} />
             <Route path="/product/:id" element={<ProductDetail />} />
-
           </Routes>
           <Footer />
-
         </BrowserRouter>
       </>
-      </CartProvider>
+    </CartProvider>
   );
 }
-
-
-
-
 
 export default App;
